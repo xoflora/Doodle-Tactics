8a09aed3cb472a0f0ba62dfd70650c6cdf341158
czchapma <christine_chapman@brown.edu>
2012-04-23 21:17:16 -0400

    events

--- doodleTactics/src/event/DialogueBox.java
+++ doodleTactics/src/event/DialogueBox.java
@@ -22,11 +22,11 @@ import main.DoodleTactics;
  * a dialogue instance controls a dialogue between characters
  */
 public class DialogueBox extends Event {
-	
+
 	private List<String> _phrases;
 	private List<Character> _characters;
-	
-	
+
+
 	/**
 	 * Creates a DialogueBox by parsing a dialogue csv file in the following format
 	 * name, phrase 1
@@ -39,105 +39,59 @@ public class DialogueBox extends Event {
 	 */
 	public DialogueBox(DoodleTactics dt, String filename, HashMap<String,Character> allChars) throws InvalidEventException, IOException, FileNotFoundException{
 		super(dt);
-		
+
 		BufferedReader br = new BufferedReader(new FileReader(filename));
-		 _phrases = new LinkedList<String>();
-		 _characters = new LinkedList<Character>();
+		_phrases = new LinkedList<String>();
+		_characters = new LinkedList<Character>();
 		String line = br.readLine();
 		String[] split;
 		while(line != null){
 			split = line.split(",");
 			if(split.length != 2)
 				throw new InvalidEventException(line);
-			
+
 			Character c = allChars.get(split[0].trim());
 			//throw error if Character not found
 			if(c == null)
 				throw new InvalidEventException(split[0], line);
 			_characters.add(c);
-			
+
 			_phrases.add(split[1].trim());
 			line = br.readLine();
 		}
 	}
-	
+
 	public void print(){
 		for(int i=0; i<_phrases.size(); i++){
 			System.out.println(_characters.get(i).getName() + ": " + _phrases.get(i));
 		}
 	}
-	
+
 	//getters
 	public List<Character> getCharList(){
 		return _characters;
 	}
-	
+
 	public List<String> getDialogueList(){
 		return _phrases;
-	}
-	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	
-	public void display() {
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		switch (e.getKeyCode()){
+		case KeyEvent.VK_UP:
+			//handle up
+			break;
+		case KeyEvent.VK_DOWN:
+			//handle down
+			break;
+		}
 	}
 
-	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+
+	public void display() {
+
 	}
 
-	@Override
-	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
+
 }
