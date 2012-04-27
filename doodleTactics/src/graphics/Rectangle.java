29c987dce1cbff067f8430a188f2dea85f1406d5
czchapma <christine_chapman@brown.edu>
2012-04-26 23:12:27 -0400

    aijhrfaihtg

--- doodleTactics/src/graphics/Rectangle.java
+++ doodleTactics/src/graphics/Rectangle.java
@@ -19,6 +19,8 @@ public abstract class Rectangle extends Shape {
 
 		@Override
 		public int compare(Rectangle r1, Rectangle r2) {
+			if(r1.getPaintPriority() == r2.getPaintPriority())
+				return (int)( r1.getX() - r2.getX());
 			return r1.getPaintPriority() - r2.getPaintPriority();
 		}
 	}
