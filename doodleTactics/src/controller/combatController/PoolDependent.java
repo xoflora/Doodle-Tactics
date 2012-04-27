d6e173ba500ac539ad37fe7571d624013b51225e
Ryan Roelke <ryan_roelke@brown.edu>
2012-04-27 00:32:58 -0400

    working with unit pool

--- doodleTactics/src/controller/combatController/PoolDependent.java
+++ doodleTactics/src/controller/combatController/PoolDependent.java
@@ -33,4 +33,9 @@ public interface PoolDependent {
 	 */
 	public void addCharacterToPool(Character c);
 	
+	
+	/**
+	 * finalizes actions with the unit pool to indicate that it is no longer needed
+	 */
+	public void finalize();
 }
