34a633fa32be913a1a6b41722c660b49c21614b3
Joseph Shapiro <Joseph_Shapiro@brown.edu>
2012-04-29 13:46:12 -0400

    yay

--- doodleTactics/src/controller/GameScreenController.java
+++ doodleTactics/src/controller/GameScreenController.java
@@ -4,8 +4,13 @@ import graphics.MenuItem;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 import java.util.List;
+
+import javax.swing.JPanel;
+
+import controller.combatController.UnitStatMenu;
 
 import util.Util;
 
@@ -21,11 +26,11 @@ import character.Character;
 public abstract class GameScreenController extends Controller {
 	
 	protected GameScreen _gameScreen;
-		
 	private int _draggedx;
 	private int _draggedy;
 	
 	protected Tile _hoveredTile;
+	protected UnitStatMenu _unitStats;
 	
 	public GameScreenController(DoodleTactics dt) {
 		super(dt);
@@ -35,6 +40,7 @@ public abstract class GameScreenController extends Controller {
 		_draggedy = 0;
 		
 		_hoveredTile = null;
+		_unitStats = null;
 	}
 	
 	@Override
@@ -85,6 +91,8 @@ public abstract class GameScreenController extends Controller {
 			_draggedx = e.getX();
 			_draggedy = e.getY();
 		}
+		
+		this.removeUnitStats();
 	}
 	
 	/**
@@ -101,6 +109,20 @@ public abstract class GameScreenController extends Controller {
 				_hoveredTile.setHovered(true);
 				_gameScreen.repaint();
 			}
+			
+			if (t.isOccupied()) {
+				Character c = t.getOccupant();
+				
+					this.removeUnitStats();
+				
+					_unitStats = new UnitStatMenu(_gameScreen, _dt.importImage("src/graphics/menu/unit_stats_box.png"),
+							_dt.importImage("src/graphics/menu/unit_stats_box.png"), _dt, c);
+					_gameScreen.addMenuItem(_unitStats);
+					_unitStats.setLocation(c.getX() + 40, c.getY() - 60);
+					_unitStats.setVisible(true);
+			} else {
+					this.removeUnitStats();
+			}
 		}
 		
 		_gameScreen.checkContains(e.getPoint());
@@ -116,6 +138,15 @@ public abstract class GameScreenController extends Controller {
 			synchronized(_clickedButton) {
 				_clickedButton.activate(e.getButton());
 			}
+		}
+	}
+	
+	public void removeUnitStats() {
+		if(_unitStats != null) {
+			_gameScreen.removeMenuItem(_unitStats);
+			_unitStats.setVisible(false);
+			_unitStats = null;
+			_gameScreen.repaint();
 		}
 	}
 	
--- doodleTactics/src/controller/combatController/UnitStatMenu.java
+++ doodleTactics/src/controller/combatController/UnitStatMenu.java
@@ -1,6 +1,9 @@
 package controller.combatController;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
@@ -12,18 +15,37 @@ import graphics.MenuItem;
 
 public class UnitStatMenu extends MenuItem {
 
+	private static int LEFT_MARGIN = 10;
+	private static int TOP_MARGIN = 10;
+	private static int BUFFER = 16;
 	private Character _character;
+	private MenuItem _profile;
 	
 	public UnitStatMenu(JPanel container, BufferedImage defltPath,
 			BufferedImage hoveredPath, DoodleTactics dt, Character c) {
 		super(container, defltPath, hoveredPath, dt);
 		_character = c;
+		_profile = new MenuItem(container, _character.getProfileImage(),_character.getProfileImage(), dt);
 	}
 	
 	@Override
 	public void paint(Graphics2D brush, BufferedImage img) {
 		super.paint(brush, img);
+		_profile.paint(brush, img);
+		brush.setRenderingHint(
+				RenderingHints.KEY_TEXT_ANTIALIASING,
+				RenderingHints.VALUE_TEXT_ANTIALIAS_GASP);
+		brush.setFont(new Font("Arial", Font.BOLD, 14));
+		brush.setColor(new Color(0,0,1));
+		brush.drawString(_character.getName() + ", Lvl " + _character.getLevel(), (int) this.getX() + LEFT_MARGIN, (int) (this.getY() + BUFFER));
+		brush.drawString("HP: " + _character.getHP() + "/" + _character.getBaseStats()[7], (int) this.getX() + LEFT_MARGIN, (int) (this.getY() + BUFFER*2));
+		brush.drawString("Atk: " + _character.getBaseStats()[0] + ", Def:" + _character.getBaseStats()[1], (int) this.getX() + LEFT_MARGIN, (int) (this.getY() + BUFFER*3));
 	}
 	
-
+	@Override
+	public void setLocation(double x, double y) {
+		super.setLocation(x, y);
+		_profile.setLocation(x + LEFT_MARGIN, y + TOP_MARGIN);
+		
+	}
 }
