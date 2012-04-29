06e18d85f5c74bafeda4ae7410909a0c961546f4
Ryan Roelke <ryan_roelke@brown.edu>
2012-04-29 04:48:45 -0400

    player combat related stuff moved to its own package

---
+++ doodleTactics/src/controller/combatController/player/PlayerCombatController.java
@@ -0,0 +1,491 @@
+package controller.combatController.player;
+
+import graphics.MenuItem;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import controller.combatController.ActionType;
+import controller.combatController.CombatController;
+
+
+import util.Util;
+
+import main.DoodleTactics;
+import map.*;
+import character.Character;
+
+/**
+ * 
+ * @author rroelke
+ * 
+ */
+public class PlayerCombatController extends CombatController implements PoolDependent {
+	
+	private enum State {
+		START,
+		CHARACTER_SELECTED,
+		CHARACTER_MOVING,
+		CHARACTER_OPTION_MENU
+	}
+	
+	private Tile _destTile;
+	private Tile _selectedTile;
+	private Character _selectedCharacter;
+	
+	private List<Tile> _selectedMovementRange;
+	private List<Tile> _enemyAttackRange;
+	private List<Tile> _characterAttackRange;
+	
+	private List<Tile> _cacheMovementRange;
+	private List<Tile> _cacheAttackRange;
+	
+	private List<Tile> _path;
+	private int _pathCost;
+	private List<Tile> _cachePath;
+	private int _cacheCost;
+	private Tile _cacheDestTile;
+	
+	private int _menuDraggedx;
+	private int _menuDraggedy;
+	private boolean _draggingMenu;
+	
+	private State _state;
+	private UnitPool _pool;
+	private CombatOptionWindow _optionWindow;
+	private boolean _finalized;
+	
+	public PlayerCombatController(DoodleTactics dt, List<Character> units) {
+		super(dt, units);
+
+		_destTile = null;
+		_selectedTile = null;
+		_selectedCharacter = null;
+		
+		_selectedMovementRange = new ArrayList<Tile>();
+		_enemyAttackRange = new ArrayList<Tile>();
+		_characterAttackRange = new ArrayList<Tile>();
+		
+		_path = new ArrayList<Tile>();
+		_pathCost = 0;
+		_cachePath = null;
+		_cacheCost = 0;
+		_cacheDestTile = null;
+		
+		_cacheMovementRange = null;
+		_cacheAttackRange = null;
+		
+		_pool = null;
+		_optionWindow = null;
+		
+		_menuDraggedx = 0;
+		_menuDraggedy = 0;
+		_draggingMenu = false;
+		
+		clear();
+	}
+	
+	/**
+	 * sets the current movement path
+	 * @param source the starting tile
+	 * @param dest the ending tile
+	 */
+	private void setPath(Tile source, Tile dest) {
+		clearPath();
+		_path = _gameScreen.getMap().getPath(source, dest);
+		for (Tile t : _path) {
+			t.setInMovementPath(true);
+			_pathCost += t.cost();
+		}
+	}
+	
+	/**
+	 * sets the player attack range
+	 * @param source the source tile
+	 * @param c the character
+	 */
+	private void setPlayerAttackRange(Tile source, Character c) {
+		clearPlayerAttackRange();
+		_characterAttackRange = _gameScreen.getMap().getAttackRange(source, 0,
+				c.getMinAttackRange(), c.getMaxAttackRange());
+		for (Tile t : _characterAttackRange)
+			t.setInPlayerAttackRange(true);
+	}
+	
+	/**
+	 * resets the player combat controller such that nothing is selected
+	 */
+	private void clear() {
+		for (Tile t : _enemyAttackRange)
+			t.setInEnemyAttackRange(false);
+		
+		_selectedTile = null;
+		_selectedCharacter = null;
+		_destTile = null;
+		_enemyAttackRange = new ArrayList<Tile>();
+
+		clearMovementRange();
+		clearPlayerAttackRange();
+		clearPath();
+		
+		_cacheMovementRange = null;
+		_cacheAttackRange = null;
+		_cachePath = null;
+		_cacheCost = 0;
+		_cacheDestTile = null;
+		
+		if (_optionWindow != null)
+			_optionWindow.removeFromDrawingQueue();
+		_optionWindow = null;
+		_state = State.START;
+	}
+	
+	private void clearMovementRange() {
+		for (Tile t : _selectedMovementRange)
+			t.setInMovementRange(false);
+		_selectedMovementRange = new ArrayList<Tile>();
+	}
+	
+	private void clearPlayerAttackRange() {
+		for (Tile t : _characterAttackRange)
+			t.setInPlayerAttackRange(false);
+		_characterAttackRange = new ArrayList<Tile>();
+	}
+	
+	private void clearPath() {
+		for (Tile t : _path)
+			t.setInMovementPath(false);
+		_path = new ArrayList<Tile>();
+		_pathCost = 0;
+	}
+	
+	@Override
+	public void move(Character c, Tile source, List<Tile> path) {
+		super.move(c, source, path);
+		
+		try {
+			_optionWindow = new CombatOptionWindow(_dt, _gameScreen, false,
+					_selectedCharacter.getInventory().size() != 0, false, this);
+			_optionWindow.setLocation(_destTile.getX() + Tile.TILE_SIZE, _destTile.getY() - Tile.TILE_SIZE);
+			_optionWindow.addToDrawingQueue();
+			
+		} catch(IOException e) {
+			_dt.error("Error finding combat window files.");
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		super.mouseClicked(e);
+
+		if (e.getButton() == MouseEvent.BUTTON1) {
+			Tile t = _gameScreen.getTile(e.getX(), e.getY());
+			if (t != null) {
+				Character c = t.getOccupant();
+				if (_state == State.START) {
+					if (t.isOccupied()) {
+						if (c.getAffiliation() == this && !hasMoved(c)) {
+							_selectedTile = t;
+							_selectedCharacter = c;
+							
+							List<Tile> mv = _gameScreen.getMap().getMovementRange(t, c.getMovementRange());
+							List<Tile> atk = _gameScreen.getMap().getAttackRange(t, c.getMovementRange(),
+												c.getMinAttackRange(), c.getMaxAttackRange());
+							
+							_selectedMovementRange = mv;
+							_characterAttackRange = Util.difference(atk, mv);
+							
+							for (Tile toPaint : _selectedMovementRange)
+								toPaint.setInMovementRange(true);
+							for (Tile toPaint : _characterAttackRange) {
+								toPaint.setInPlayerAttackRange(true);
+							}
+							_state = State.CHARACTER_SELECTED;
+						}
+						else if (_enemyAffiliations.contains(c.getAffiliation())) {
+							_enemyAttackRange = Util.union(_enemyAttackRange,
+									_gameScreen.getMap().getAttackRange(t, c.getMovementRange(),
+											c.getMinAttackRange(), c.getMaxAttackRange()));
+						}
+					}
+				}
+				//selected tile and character are not null; selected character is the occupant of the tile
+				else if (_state == State.CHARACTER_SELECTED) {
+					if (_path.contains(t)) {
+						_destTile = t;
+						_state = State.CHARACTER_MOVING;
+						
+						move(_selectedCharacter, _selectedTile, _path);
+						
+						_cacheMovementRange = _selectedMovementRange;
+						clearMovementRange();
+						
+						_cacheAttackRange = _characterAttackRange;
+						setPlayerAttackRange(_destTile, _selectedCharacter);
+						
+						_cachePath = _path;
+						_cacheCost = _pathCost;
+						_cacheDestTile = _destTile;
+						clearPath();
+						
+						_state = State.CHARACTER_OPTION_MENU;
+					}
+				}
+				//selected tile is the coordinate to move the character to
+				else if (_state == State.CHARACTER_MOVING) {
+					
+				}
+				else if (_state == State.CHARACTER_OPTION_MENU) {
+				//	_pool.removeCharacter(_selectedCharacter);
+				//	clear();
+				//	_state = State.START;
+					if (_characterAttackRange.contains(t) && isEnemy(t.getOccupant())) {
+						attack(_selectedCharacter, t.getOccupant());
+						
+						clear();
+						_state = State.START;
+					}
+				}
+			}
+		}
+		else if (e.getButton() == MouseEvent.BUTTON3) {
+			if (_state == State.START)
+				clear();
+			else if (_state == State.CHARACTER_SELECTED)
+				clear();
+			else if (_state == State.CHARACTER_MOVING) {
+				clearPath();
+				_state = State.CHARACTER_SELECTED;
+			}
+			else if (_state == State.CHARACTER_OPTION_MENU) {
+				_destTile.setOccupant(null);
+				_selectedTile.setOccupant(_selectedCharacter);
+				_selectedCharacter.setLocation(_selectedTile.getX(), _selectedTile.getY());
+				clearPath();
+				clearPlayerAttackRange();
+				
+				_selectedMovementRange = _cacheMovementRange;
+				for (Tile toPaint : _selectedMovementRange)
+					toPaint.setInMovementRange(true);
+				_cacheMovementRange = null;
+				
+				_characterAttackRange = _cacheAttackRange;
+				for (Tile toPaint : _characterAttackRange)
+					toPaint.setInPlayerAttackRange(true);
+				_cacheAttackRange = null;
+				
+				_path = _cachePath;
+				_pathCost = _cacheCost;
+				_destTile = _cacheDestTile;
+				for (Tile toPaint : _path)
+					toPaint.setInMovementPath(true);
+				_cachePath = null;
+				_cacheCost = 0;
+				_cacheDestTile = null;
+				
+				_hasMoved.put(_selectedCharacter, false);
+				_optionWindow.removeFromDrawingQueue();
+				_optionWindow = null;
+				
+				_state = State.CHARACTER_SELECTED;
+			}
+		}
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		super.mouseMoved(e);
+		Tile t = _gameScreen.getTile(e.getX(), e.getY());
+		if (t != null) {
+			
+			if (_state == State.CHARACTER_SELECTED) {	//selected character and tile are not null
+				if (_selectedMovementRange.contains(t)) {
+					if (_path.isEmpty()) {
+						if (t.isAdjacent(_selectedTile) && t.cost() <= _selectedCharacter.getMovementRange()) {
+							_path.add(t);
+							_pathCost += t.cost();
+							t.setInMovementPath(true);
+						}
+						else
+							setPath(_selectedTile, t);
+					}
+					else if (!_path.contains(t)) {
+						if (t.isAdjacent(_path.get(_path.size() - 1)) && _pathCost + t.cost() <=
+								_selectedCharacter.getMovementRange()) {
+							_path.add(t);
+							_pathCost += t.cost();
+							t.setInMovementPath(true);
+						}
+						else {	//unreachable from current path - change entirely
+							clearPath();
+							setPath(_selectedTile, t);
+						}
+					}
+					else {	//backtracing in path
+						int i = _path.size() - 1;
+						while (_path.get(i) != t && i >= 0) {
+							_path.get(i).setInMovementPath(false);
+							_path.remove(i);
+							i--;
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@Override
+	/**
+	 * 
+	 */
+	public void mousePressed(MouseEvent e) {
+		super.mousePressed(e);
+		MenuItem m = _gameScreen.checkContains(e.getPoint());
+		if (m != null && m == _optionWindow) {
+			_menuDraggedx = e.getX();
+			_menuDraggedy = e.getY();
+			_draggingMenu = true;
+		}
+	}
+	
+	@Override
+	/**
+	 * 
+	 */
+	public void mouseDragged(MouseEvent e) {
+		if (!_draggingMenu)
+			super.mouseDragged(e);
+		else if (_optionWindow != null) {
+			_optionWindow.setLocation(e.getX() - _menuDraggedx, e.getY() - _menuDraggedy);
+			_menuDraggedx = e.getX();
+			_menuDraggedy = e.getY();
+		}
+	}
+	
+	@Override
+	/**
+	 * 
+	 */
+	public void mouseReleased(MouseEvent e) {
+		super.mouseReleased(e);
+		_draggingMenu = false;
+	}
+
+	@Override
+	/**
+	 * ends the player's turn
+	 */
+	public void release() {
+		// TODO Auto-generated method stub
+		super.release();
+		
+		clear();
+	}
+
+	@Override
+	/**
+	 * begins the player's turn
+	 */
+	public void take() {
+		super.take();
+		System.out.println("Player phase!");
+		
+		initialize();
+	}
+
+	@Override
+	/**
+	 * pans the camera to focus on the given character
+	 */
+	public void getCharacterFromPool(Character c) {
+		// TODO Auto-generated method stub
+		double x = c.getX();
+		double y = c.getY();
+		//pan to coordinates
+	}
+
+	@Override
+	public UnitPool getPool() {
+		return _pool;
+	}
+
+	@Override
+	/**
+	 * unused
+	 */
+	public void addUnitToPool(Character c) { }
+	
+	@Override
+	/**
+	 * pans the map to focus on the given character
+	 */
+	public void alternateAction(Character c) {
+		getCharacterFromPool(c);
+	}
+
+	@Override
+	/**
+	 * 
+	 */
+	public void removeUnitFromPool(Character c) {
+		_pool.removeCharacter(c);
+	}
+	
+	@Override
+	/**
+	 * 
+	 */
+	public void initialize() {
+		try {
+			_pool = new UnitPool(_dt, _gameScreen, this, _units);
+			_pool.setInUse(true);
+		} catch (IOException e) {
+			_dt.error("");
+		}
+	}
+	
+	@Override
+	/**
+	 * used at the end of the player's turn
+	 */
+	public void finalize() {
+		if (!_finalized) {
+			System.out.println("HIII");
+			_finalized = true;
+			_pool.setInUse(false);
+			_pool = null;
+			_gameScreen.popControl();
+			_finalized = false;
+		}
+	}
+
+	/**
+	 * sends an action to the controller
+	 * @param action the action to send
+	 */
+	public void pushAction(ActionType action) {
+		assert(_state == State.CHARACTER_OPTION_MENU);
+		
+		if (action == ActionType.WAIT) {
+			_pool.removeCharacter(_selectedCharacter);
+			clear();
+			System.out.println(_pool.getNumUnits());
+			if (_pool.isEmpty()) {
+				_pool.setInUse(false);
+				_pool = null;
+				_gameScreen.popControl();
+			}
+			else
+				_state = State.START;
+		}
+		else if (action == ActionType.ITEM) {
+			
+		}
+		else if (action == ActionType.SPECIAL) {
+			
+		}
+	}
+}
---
+++ doodleTactics/src/controller/combatController/player/UnitPool.java
@@ -0,0 +1,217 @@
+package controller.combatController.player;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.PriorityQueue;
+
+import javax.swing.JPanel;
+
+
+import main.DoodleTactics;
+import main.GameScreen;
+import map.Tile;
+
+import graphics.MenuItem;
+import graphics.Rectangle;
+import character.Character;
+
+/**
+ * a unit pool is a graphical interface that allows players to select units
+ * @author rroelke
+ *
+ */
+public class UnitPool extends MenuItem {
+
+	private static final int SIDEBAR_WIDTH = Tile.TILE_SIZE;
+	private static final int SIDEBAR_HEIGHT = 160;
+
+	private static final int CHARACTER_OFFSET_X = 40;
+	private static final int CHARACTER_OFFSET_Y = 120;
+	private static final int OFFSET_BETWEEN_CHARACTERS = Tile.TILE_SIZE + 4;
+
+	private static final int DEFAULT_X = 0;
+	private static final int DEFAULT_Y = 0;
+	private static final int DONE_X = 11;
+	private static final int DONE_Y = 746;
+
+	public static final int SIDEBAR_PRIORITY = 0;
+	public static final int CHARACTER_SELECT_PRIORITY = 1;
+	public static final int DONE_BUTTON_PRIORITY = 1;
+
+	public static final int SELECT_BUTTON = MouseEvent.BUTTON1;
+	public static final int ALT_BUTTON = MouseEvent.BUTTON3;
+
+	private static final String IMAGE_PATH = "src/graphics/menu/sidebar.png";
+	private static final String DONE_DEFAULT_IMG = "src/graphics/menu/done.png";
+	private static final String DONE_HOVER_IMG = "src/graphics/menu/done_hovered.png";
+
+	/**
+	 * corresponds to a character button in the unit pool
+	 * @author rroelke
+	 *
+	 */
+	private class CharacterSelect extends MenuItem {
+
+		private Character _c;
+
+		public CharacterSelect(JPanel container, BufferedImage defltPath,
+				BufferedImage hoveredPath, DoodleTactics dt, Character c) {
+			super(container, defltPath, hoveredPath, dt, CHARACTER_SELECT_PRIORITY);
+			setVisible(true);
+			_c = c;
+		}
+
+		@Override
+		public void activate(int type) {
+			//	System.out.println("activating on character " + _c);
+			if (type == SELECT_BUTTON)
+				_source.getCharacterFromPool(_c);
+			else if (type == ALT_BUTTON)
+				_source.alternateAction(_c);
+		}
+	}
+
+	/**
+	 * menu item indicating that the unit pool is no longer required
+	 * @author rroelke
+	 */
+	private class DoneButton extends MenuItem {
+
+		private boolean _complete;
+
+		public DoneButton(JPanel container, BufferedImage defltPath,
+				BufferedImage hoveredPath, DoodleTactics dt) {
+			super(container, defltPath, hoveredPath, dt, DONE_BUTTON_PRIORITY);
+			_complete = false;
+			setVisible(true);
+		}
+
+		@Override
+		public void activate(int type) {
+			synchronized(this) {
+				if (!_complete) {
+					_complete = true;
+					_source.finalize();
+				}
+			}
+		}
+	}
+
+	private PoolDependent _source;
+	private HashMap<Character, CharacterSelect> _unitPool;
+	private DoneButton _done;
+	private GameScreen _gameScreen;
+
+	private boolean _inUse;
+	private int _numUnits;
+
+	public UnitPool(DoodleTactics dt, GameScreen screen, PoolDependent source, List<Character> units) 
+			throws IOException {
+		super(screen, dt.importImage(IMAGE_PATH), dt.importImage(IMAGE_PATH), dt, SIDEBAR_PRIORITY);
+
+		_dt = dt;
+		_source = source;
+		_unitPool = new HashMap<Character, CharacterSelect>();
+		_gameScreen = screen;
+		_numUnits = 0;
+		_inUse = false;
+
+		for (Character unit : units)
+			addCharacter(unit);
+		_done = new DoneButton(_gameScreen, dt.importImage(DONE_DEFAULT_IMG),
+				dt.importImage(DONE_HOVER_IMG), _dt);
+		_done.setLocation(DONE_X, DONE_Y);
+		_done.setVisible(true);
+
+		setLocation(DEFAULT_X, DEFAULT_Y);
+	}
+
+	/**
+	 * removes a unit from the unit pool
+	 * @param c the unit to remove
+	 * @return whether or not the unit was successfully removed
+	 */
+	public boolean removeCharacter(Character c) {
+		CharacterSelect s = _unitPool.remove(c);
+		if (s != null) {
+			_numUnits--;
+			if (_inUse)
+				return _gameScreen.removeMenuItem(s);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * adds a unit to the unit pool
+	 * @param c the unit to add
+	 */
+	public void addCharacter(Character c) {
+		CharacterSelect s = new CharacterSelect(_gameScreen, c.getDownImage(), c.getRightImage(), _dt, c);
+		_unitPool.put(c, s);
+		_numUnits++;
+
+		if (_inUse)
+			_gameScreen.addMenuItem(s);
+	}
+
+	@Override
+	public void paint(Graphics2D brush, BufferedImage img) {
+		super.paint(brush, img);
+
+		int x = DEFAULT_X + CHARACTER_OFFSET_X;
+		int y = DEFAULT_Y + CHARACTER_OFFSET_Y;
+
+		for (Character c : _unitPool.keySet()) {
+			CharacterSelect draw = _unitPool.get(c);
+			draw.setLocation(x, y);
+
+			y += OFFSET_BETWEEN_CHARACTERS;
+		}
+	}
+
+	/**
+	 * sets whether or not the pool should be used
+	 * @param b whether or not the pool should be used
+	 */
+	public void setInUse(boolean b) {
+		if (b && !_inUse) {
+			_gameScreen.addMenuItem(this);
+			_gameScreen.addMenuItem(_done);
+			for (Character c : _unitPool.keySet())
+				_gameScreen.addMenuItem(_unitPool.get(c));
+
+			_inUse = true;
+			setVisible(true);
+		}
+		else if (!b && _inUse){
+			for (Character c : _unitPool.keySet())
+				_gameScreen.removeMenuItem(_unitPool.get(c));
+			_gameScreen.removeMenuItem(_done);
+			_gameScreen.removeMenuItem(this);
+
+			_inUse = false;
+			setVisible(false);
+		}
+	}
+	
+	/**
+	 * @return the number of units in the character pool
+	 */
+	public int getNumUnits() {
+		return _numUnits;
+	}
+	
+	/**
+	 * @return whether there are no units remaining in the pool
+	 */
+	public boolean isEmpty() {
+		return _numUnits == 0;
+	}
+}
