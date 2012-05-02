5b49259262270ddbcfc37f6e154effcd005e67b8
czchapma <christine_chapman@brown.edu>
2012-05-02 01:09:04 -0400

    save works ish

--- doodleTactics/src/graphics/ScreenChangeMenuItem.java
+++ doodleTactics/src/graphics/ScreenChangeMenuItem.java
@@ -1,5 +1,8 @@
 package graphics;
 
+import items.HealthPotion;
+import items.ItemException;
+
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
@@ -24,6 +27,18 @@ public class ScreenChangeMenuItem extends MenuItem {
 	public void activate(int type) {		
 		_dt.changeScreens(_nextScreen);
 		_dt.getGameScreen().setMap("src/tests/data/testMapDemo");
+		//Modify Character
+		HealthPotion potion = new HealthPotion(_dt.importImage("src/graphics/items/donut.png"), 10);
+		potion.setDescription("Sup bro");
+		try {
+			_dt.getParty().get(1).addToInventory(potion);
+			_dt.getParty().get(1).updateHP(-10);
+
+		} catch (ItemException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		_nextScreen.repaint();
 
 	}
