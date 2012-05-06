d2180297b27b0f697380d796f309cb20f2a93c5d
czchapma <christine_chapman@brown.edu>
2012-05-06 00:40:01 -0400

    yes

--- doodleTactics/src/event/CombatEvent.java
+++ doodleTactics/src/event/CombatEvent.java
@@ -4,9 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 import main.DoodleTactics;
+import map.Tile;
+import character.Archer;
+import character.Character;
+import character.Mage;
+import character.Thief;
+import character.Warrior;
 
 public class CombatEvent extends Event{
 
@@ -19,6 +26,13 @@ public class CombatEvent extends Event{
 		ROOT,
 		SURVIVE
 	}
+
+	public enum FactionType{
+		Enemy,
+		Neutral,
+		Partner
+
+	}
 	/**
 	 * 
 	 * @param filepath - file to parse
@@ -30,10 +44,13 @@ public class CombatEvent extends Event{
 	public CombatEvent parseEvent(String filepath) throws InvalidEventException{
 		WinCondition wc;
 		LinkedList<Character> _combatants;
+		int numUnits;
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(filepath));
+			numUnits = Integer.parseInt(br.readLine());
 			String line = br.readLine();
-			//Chec
+
+			//Check Win Conditions
 			int numRounds = -1;
 			if(line.startsWith("root")){
 				wc = WinCondition.ROOT;
@@ -41,17 +58,49 @@ public class CombatEvent extends Event{
 				wc = WinCondition.SURVIVE;
 				numRounds = Integer.parseInt(line.split(",")[1]);
 			}
-			
-			//Looping through types:
-		/*	HashMap<String,Type>
-			
+
+			//Get faction types
+			HashMap<String,FactionType> groupNameToType = new HashMap<String,FactionType>();
+			HashMap<String,HashMap<Character,Tile>> nameToCharacters = new HashMap<String,HashMap<Character,Tile>>();
+			String[] split = br.readLine().split(",");
+			while(split.length == 2){
+				groupNameToType.put(split[1], getFactionType(split[0]));
+				nameToCharacters.put(split[1], new HashMap<Character,Tile>());
+				split = br.readLine().split(",");
+			}
+
 			//Loop through Characters
-			line = br.readLine();*/
+			line = br.readLine();
 			while(line != null){
-				//Map Character
-				if(!line.contains(",")){
-					
-				}
+				split = line.split(",");
+				
+				//Map Character:
+				//faction,name,tileX,tileY
+				if(split.length == 4){
+					String group = split[0];
+					Character toAdd = _dt.getCharacter(split[1]);
+					Tile t = _dt.getGameScreen().getMap().getTile(Integer.parseInt(split[2]), Integer.parseInt(split[3]));
+					nameToCharacters.get(group).put(toAdd, t);
+				} else if(split.length == 7){
+					String group = split[0];
+					String type = split[0];
+					int x = Integer.parseInt(split[7]);
+					int y = Integer.parseInt(split[8]);
+					Character c;
+					if(type.equalsIgnoreCase("mage"))
+						c = new Mage(_dt,_dt.getGameScreen(),split[2],split[3],split[4],split[5],split[6],"Mage",x,y);
+					else if(type.equalsIgnoreCase("warrior"))
+						c = new Warrior(_dt,_dt.getGameScreen(),split[2],split[3],split[4],split[5],split[6],"Warrior",x,y);
+					else if(type.equalsIgnoreCase("archer"))
+						c = new Archer(_dt,_dt.getGameScreen(),split[2],split[3],split[4],split[5],split[6],"Archer",x,y);
+					else if(type.equalsIgnoreCase("thief"))
+						c = new Thief(_dt,_dt.getGameScreen(),split[2],split[3],split[4],split[5],split[6],"Mage",x,y);
+					else
+						throw new InvalidEventException("CombatEvent- expected Character class, received: " + type);
+					Tile t = _dt.getGameScreen().getMap().getTile(x, y);
+					nameToCharacters.get(group).put(c, t);
+				} else
+					throw new InvalidEventException("CombatEvent- incorrect number of items on line");
 				line = br.readLine();
 			}
 		} catch (FileNotFoundException e) {
@@ -60,8 +109,20 @@ public class CombatEvent extends Event{
 			throw new InvalidEventException("Something went wrong while reading " + filepath);
 
 		}
-		
+
 		return null;
+	}
+
+
+	public FactionType getFactionType(String s){
+		if(s.equalsIgnoreCase("enemy"))
+			return FactionType.Enemy;
+		else if(s.equalsIgnoreCase("partner"))
+			return FactionType.Enemy;
+		else if (s.equalsIgnoreCase("neutral"))
+			return FactionType.Neutral;
+		else
+			return null;
 	}
 
 
